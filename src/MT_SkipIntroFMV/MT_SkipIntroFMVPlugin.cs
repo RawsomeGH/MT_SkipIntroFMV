29022de1151d97a0a2b11ffcfd1dc8400ab0a2f6
Bernhard Pasching <bernhard.pasching@gmail.com>
2020-06-01 23:15:18 +0200

    Create solution, README and patch

---
+++ src/MT_SkipIntroFMV/MT_SkipIntroFMVPlugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MT_SkipIntroFMV.Config;
+
+namespace MT_SkipIntroFMV
+{
+    [BepInPlugin("rawsome.modster-train.skip-intro-fmv", "Skip Intro FMV", "1.0.0.0")]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    class MT_SkipIntroFMVPlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            LoadConfig();
+
+            // If disabled, don't patch at all.
+            if (MT_SkipIntroFMVConfig.SkipIntroFMV.Value)
+            {
+                var harmony = new Harmony("rawsome.modster-train.skip-intro-fmv");
+                harmony.PatchAll();
+            }
+        }
+
+        void LoadConfig()
+        {
+            MT_SkipIntroFMVConfig.SkipIntroFMV = Config.Bind("General", "Enabled", true, $"Whether intro FMV should be skipped ('true' to skip, otherwise 'false')");
+        }
+    }
+}
---
+++ src/MT_SkipIntroFMV/Patches/IntroFmvScreen_Initialize_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace MT_SkipIntroFMV.Patches
+{
+    /// <summary>
+    /// Patches IntroFmvScreen.Initialize to not play the video (namespace: - [in Assembly-Csharp.dll])
+    /// </summary>
+    [HarmonyPatch(typeof(IntroFmvScreen), "Initialize")]
+    static class IntroFmvScreen_Initialize_Patch
+    {
+        static bool Prefix(ref IntroFmvScreen __instance)
+        {
+            // Before Initialize is executed, invoke the method that would be called once playback has ended.
+            __instance.Invoke("GoToNextScreen", 0);
+
+            // Return false to prevent the original Initialize method from being executed.
+            return false;
+        }
+    }
+}
