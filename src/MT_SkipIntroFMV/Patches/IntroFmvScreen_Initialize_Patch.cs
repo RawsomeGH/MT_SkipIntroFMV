d65054f0a036d22149124a29f6908d5597882358
Bernhard Pasching <bernhard.pasching@gmail.com>
2020-09-04 13:19:04 +0200

    Fixed the patch to work with the Friends And Foes update

--- src/MT_SkipIntroFMV/Patches/IntroFmvScreen_Initialize_Patch.cs
+++ src/MT_SkipIntroFMV/Patches/IntroFmvScreen_Initialize_Patch.cs
@@ -6,14 +6,28 @@ namespace MT_SkipIntroFMV.Patches
     /// Patches IntroFmvScreen.Initialize to not play the video (namespace: - [in Assembly-Csharp.dll])
     /// </summary>
     [HarmonyPatch(typeof(IntroFmvScreen), "Initialize")]
-    static class IntroFmvScreen_Initialize_Patch
+    class IntroFmvSkip
     {
-        static bool Prefix(ref IntroFmvScreen __instance)
+        static bool Prefix(IntroFmvScreen __instance, SaveManager ___saveManager, GameStateManager ___gameStateManager)
         {
-            // Before Initialize is executed, invoke the method that would be called once playback has ended.
-            __instance.Invoke("GoToNextScreen", 0);
+            if (___saveManager.GetHasSeenFtueScenario())
+            {
+                // _videoPlayer needs to be initialized for EndReached
+                var videoPlayerField = AccessTools.Field(typeof(IntroFmvScreen), "_videoPlayer");
 
-            // Return false to prevent the original Initialize method from being executed.
+                videoPlayerField
+                    .SetValue(__instance, __instance.gameObject.AddComponent<ShinyShoe.VideoPlayerUnity>());
+
+                AccessTools.Method(typeof(IntroFmvScreen), "EndReached")
+                    .Invoke(__instance, new object[] { null });
+            }
+            else if (___gameStateManager != null)
+            {
+                // If player has not seen Ftue, start a new run once GameStateManager has been injected
+                ___gameStateManager.StartNewRun(RunType.Class, string.Empty, null, true);
+            }
+            
+            // Prevent actual Initialize from happening
             return false;
         }
     }
